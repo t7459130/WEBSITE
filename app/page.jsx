d1368f3a4e79a0a3073312d083bb1dfeb15bf3a6
Rehan Dar <rehandar@windsorgreatcars.com>
2026-05-22 03:06:39 +0100

    OK SHOULD IS NOW 2

--- app/page.jsx
+++ app/page.jsx
@@ -39,9 +39,8 @@ export default function Home() {
 
   const extractVideoId = url => {
 
-    const regExp =
-      /^.*(youtu.be\\/|v\\/|u\\/\\w\\/|embed\\/|watch\\?v=|\\&v=)([^#\\&\\?]*).*/
-
+     const regExp =
+         /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
     const match = url.match(regExp)
 
     return match && match[2].length === 11
